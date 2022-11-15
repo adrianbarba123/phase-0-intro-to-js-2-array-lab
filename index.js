cc3ba911cccd704cdd9c4c2026bd2a23f70d32cf
Adrian Barba <adrianbarba123@gmail.com>
2022-11-15 12:28:25 -0800

    commit tree 3f0c853ed508c1b2133846178be4aa653bfc0feb

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const temp = [...cats, name];
+    return temp;
+}
+
+function prependCat(name){
+    const temp = [name, ...cats];
+    return temp;
+}
+
+function removeLastCat(){
+    const temp = [...cats];
+    temp.pop();
+    return temp;
+}
+
+function removeFirstCat(){
+    const temp = [...cats];
+    temp.shift();
+    return temp;
+}
